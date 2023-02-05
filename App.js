5a5ea07c91c9633e1ac585db1732f8df48556d52
David Bobe <bobe.david@live.com>
2023-02-05 13:56:14 -0500

    Loading component

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
+import { NativeBaseProvider } from 'native-base';
 import { StyleSheet, View } from 'react-native';
 
 import LoadingComponent from './src/components/LoadingComponent';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <LoadingComponent />
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <View style={styles.container}>
+        <LoadingComponent />
+        <StatusBar style="auto" />
+      </View>
+    </NativeBaseProvider>
   );
 }
 
--- src/components/LoadingComponent.js
+++ src/components/LoadingComponent.js
@@ -1,10 +1,17 @@
-import { View, Text } from 'react-native';
 import React from 'react';
+import { Center, HStack, Text, Image } from 'native-base';
 
 export default function LoadingComponent() {
   return (
-    <View>
-      <Text>Puerto Rico Cafe</Text>
-    </View>
+    <Center>
+      <HStack alignItems="center">
+        <Image
+          source={require('../../assets/Flag_of_Puerto_Rico.gif')}
+          alt="Puerto Rico Flag"
+          style={{ width: 48, height: 36 }}
+        />
+        <Text marginLeft={2}>Loading...</Text>
+      </HStack>
+    </Center>
   );
 }
