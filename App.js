540d08f59fce712ceaa5e1272060a427391ec248
David Bobe <bobe.david@live.com>
2023-02-05 11:40:30 -0500

    ESLint and Prettier Config

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+import LoadingComponent from './src/components/LoadingComponent';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <LoadingComponent />
       <StatusBar style="auto" />
     </View>
   );
